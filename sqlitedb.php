be0c62b3336290c97d7ba93fe2efb6f4f62fc057
MatthewRigoli <matthewrigoli@pop-os.localdomain>
2019-05-10 13:51:31 -0600

    Created a Table containing Username, Password, ClockedIn(0 or 1)

--- sqlitedb.php
+++ sqlitedb.php
@@ -10,4 +10,20 @@
    } else {
       echo "Opened database successfully\n";
    }
+
+   $sql =<<<EOF
+      CREATE TABLE USERINFO
+      (ID INT PRIMARY KEY     NOT NULL,
+      USERNAME       TEXT     NOT NULL,
+      PASSWORD       TEXT     NOT NULL,
+      CLOCKEDIN      INT      NOT NULL);
+EOF;
+
+   $ret = $db->exec($sql);
+   if(!$ret){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Table created successfully\n";
+   }
+   $db->close();
 ?>
